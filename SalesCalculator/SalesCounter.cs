eda83a8717fcd984634bfbb73df25925a84f4d4d
Yu Takazawa <32022@ojs.ac.jp>
2021-05-27 15:19:32 +0900

    P64 リスト2,17

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -13,5 +13,17 @@ namespace SalesCalculator {
         public SalesCounter(List<Sale>sales) {
             _sales = sales;
         }
+        public Dictionary<string,int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach(Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    //すでにコレクション店舗が設定されている
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    //コレクションへ店舗を登録
+                    dict[sale.ShopName] = sale.Amount;
+            }
+            return dict;
+        }
     }
 }
