3159883853286075cf28fe66dc263a45d012b831
Yu Takazawa <32022@ojs.ac.jp>
2021-06-01 09:36:59 +0900

    P79 リスト2.1.4

---
+++ Excercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise1 {
+    class Program {
+        static void Main(string[] args) {
+            var songs = new Song[] {
+                new Song("歌１","歌手１",300),
+                new Song("歌2","歌手2",200),
+                new Song("歌3","歌手3",250),
+            };
+        }
+
+        private static void PintSongs(Song[] songs) {
+            foreach(var song in songs) {
+                Console.WriteLine(@"{0},{1},{2\m:ss}",song.Title,song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
+        }
+    }
+}
---
+++ Excercise1/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise1 {
+    class Song {
+        //歌のタイトル
+        public string Title { get; set; }
+        //アーティスト名
+        public string ArtistName { get; set; }
+        //演奏時間、単位は秒
+        public int Length { get; set; }
+
+        public Song(string Title, string ArtistName, int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+    }
+}
