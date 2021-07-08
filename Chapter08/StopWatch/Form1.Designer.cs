7a9affb67dfb7ba884becc569a6bd54550909164
Yu Takazawa <32022@ojs.ac.jp>
2021-07-08 14:23:38 +0900

    ストップウォッチ 完成

--- Chapter08/StopWatch/Form1.Designer.cs
+++ Chapter08/StopWatch/Form1.Designer.cs
@@ -24,16 +24,21 @@ namespace StopWatch {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
             this.btStart = new System.Windows.Forms.Button();
             this.btStop = new System.Windows.Forms.Button();
             this.lbTimerDisp = new System.Windows.Forms.Label();
+            this.btReset = new System.Windows.Forms.Button();
+            this.btLap = new System.Windows.Forms.Button();
+            this.lbLapDisp = new System.Windows.Forms.ListBox();
+            this.tmDisp = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // btStart
             // 
-            this.btStart.Location = new System.Drawing.Point(70, 103);
+            this.btStart.Location = new System.Drawing.Point(66, 103);
             this.btStart.Name = "btStart";
-            this.btStart.Size = new System.Drawing.Size(77, 49);
+            this.btStart.Size = new System.Drawing.Size(129, 71);
             this.btStart.TabIndex = 0;
             this.btStart.Text = "スタート";
             this.btStart.UseVisualStyleBackColor = true;
@@ -41,12 +46,13 @@ namespace StopWatch {
             // 
             // btStop
             // 
-            this.btStop.Location = new System.Drawing.Point(70, 180);
+            this.btStop.Location = new System.Drawing.Point(66, 191);
             this.btStop.Name = "btStop";
-            this.btStop.Size = new System.Drawing.Size(77, 48);
+            this.btStop.Size = new System.Drawing.Size(129, 71);
             this.btStop.TabIndex = 1;
             this.btStop.Text = "ストップ";
             this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
             // 
             // lbTimerDisp
             // 
@@ -58,11 +64,49 @@ namespace StopWatch {
             this.lbTimerDisp.TabIndex = 2;
             this.lbTimerDisp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // btReset
+            // 
+            this.btReset.Location = new System.Drawing.Point(223, 103);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(129, 71);
+            this.btReset.TabIndex = 3;
+            this.btReset.Text = "リセット";
+            this.btReset.UseVisualStyleBackColor = true;
+            this.btReset.Click += new System.EventHandler(this.btReset_Click);
+            // 
+            // btLap
+            // 
+            this.btLap.Location = new System.Drawing.Point(223, 191);
+            this.btLap.Name = "btLap";
+            this.btLap.Size = new System.Drawing.Size(129, 71);
+            this.btLap.TabIndex = 4;
+            this.btLap.Text = "ラップ";
+            this.btLap.UseVisualStyleBackColor = true;
+            this.btLap.Click += new System.EventHandler(this.btLap_Click);
+            // 
+            // lbLapDisp
+            // 
+            this.lbLapDisp.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbLapDisp.FormattingEnabled = true;
+            this.lbLapDisp.ItemHeight = 16;
+            this.lbLapDisp.Location = new System.Drawing.Point(387, 29);
+            this.lbLapDisp.Name = "lbLapDisp";
+            this.lbLapDisp.Size = new System.Drawing.Size(258, 244);
+            this.lbLapDisp.TabIndex = 5;
+            // 
+            // tmDisp
+            // 
+            this.tmDisp.Interval = 10;
+            this.tmDisp.Tick += new System.EventHandler(this.tmDisp_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 366);
+            this.ClientSize = new System.Drawing.Size(708, 366);
+            this.Controls.Add(this.lbLapDisp);
+            this.Controls.Add(this.btLap);
+            this.Controls.Add(this.btReset);
             this.Controls.Add(this.lbTimerDisp);
             this.Controls.Add(this.btStop);
             this.Controls.Add(this.btStart);
@@ -78,6 +122,10 @@ namespace StopWatch {
         private System.Windows.Forms.Button btStart;
         private System.Windows.Forms.Button btStop;
         private System.Windows.Forms.Label lbTimerDisp;
+        private System.Windows.Forms.Button btReset;
+        private System.Windows.Forms.Button btLap;
+        private System.Windows.Forms.ListBox lbLapDisp;
+        private System.Windows.Forms.Timer tmDisp;
     }
 }
 
--- Chapter08/StopWatch/Form1.cs
+++ Chapter08/StopWatch/Form1.cs
@@ -14,6 +14,7 @@ namespace StopWatch {
 
         //時間計測のストップウォッチ
         Stopwatch sw = new Stopwatch();
+        
         public Form1() {
             InitializeComponent();
         }
@@ -22,9 +23,27 @@ namespace StopWatch {
             lbTimerDisp.Text = sw.Elapsed.ToString(@"hh\:mm\:ss\.ff");
         }
 
+        private void tmDisp_Tick(object sender, EventArgs e) {
+            lbTimerDisp.Text = sw.Elapsed.ToString(@"hh\:mm\:ss\.ff");
+        }
+
         private void btStart_Click(object sender, EventArgs e) {
             sw.Start();
-            
+            tmDisp.Start();
+        }
+
+        private void btStop_Click(object sender, EventArgs e) {
+            sw.Stop();
+            tmDisp.Stop();
+        }
+
+        private void btReset_Click(object sender, EventArgs e) {
+            sw.Reset();
+            lbTimerDisp.Text = sw.Elapsed.ToString(@"hh\:mm\:ss\.ff");
+        }
+
+        private void btLap_Click(object sender, EventArgs e) {
+            lbLapDisp.Items.Insert(0,sw.Elapsed.ToString(@"hh\:mm\:ss\.ff"));//Insertで古い記録は下へいく
         }
     }
 }
