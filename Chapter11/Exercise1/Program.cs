61a8e6cfa3e0e6bd2055ae394811e7356778c6a1
侑 髙澤 <32022@ojs.ac.jp>
2021-08-27 16:01:09 +0900

    P299 1_2

--- Chapter11/Exercise1/Program.cs
+++ Chapter11/Exercise1/Program.cs
@@ -14,10 +14,10 @@ namespace Exercise1
             var file = "Sample.xml";
             Exercise1_1(file);
             Console.WriteLine("-------");
-            /*var file = "Sample.xml";
-            Exercise1_2(file);
+            var file2 = "Sample.xml";
+            Exercise1_2(file2);
             Console.WriteLine("-------");
-            var file = "Sample.xml";
+            /*var file = "Sample.xml";
             Exercise1_3(file);
             Console.WriteLine("-------");*/
         }
@@ -25,7 +25,7 @@ namespace Exercise1
         private static void Exercise1_1(string file)
         {
             var xdoc = XDocument.Load("Sample.xml");
-            foreach (var sample in xdoc.Root.Elements())
+            foreach (var sample in xdoc.Root.Elements())  
             {
                 var xname = sample.Element("name");
                 var member = (int)sample.Element("teammembers");
@@ -33,12 +33,22 @@ namespace Exercise1
             }
         }
 
-        /*private static void Exercise1_2(string file)
+        private static void Exercise1_2(string file2)
         {
             var xdoc = XDocument.Load("Sample.xml");
+            var xsamples = xdoc.Root.Elements()
+                                       .Select(x => new
+                                       {
+                                           Firstplayed = x.Element("firstplayed").Value,
+                                           Name = x.Element("name").Attribute("kanji").Value
+                                       }).OrderBy(x => int.Parse(x.Firstplayed));//Order ByをOrder By Descendingにすると新しい順になる
+            foreach (var sample in xsamples)
+            {
+                Console.WriteLine("{0} {1}",sample.Name,sample.Firstplayed);
+            }
         }
 
-        private static void Exercise1_3(string file)
+        /*private static void Exercise1_3(string file)
         {
             var xdoc = XDocument.Load("Sample.xml");
         }*/
