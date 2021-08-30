6015b2f41e9a2b28b0a2bc65f2037d716f5a3071
侑 髙澤 <32022@ojs.ac.jp>
2021-08-30 13:31:31 +0900

    P299,1_3

--- Chapter11/Exercise1/Program.cs
+++ Chapter11/Exercise1/Program.cs
@@ -17,9 +17,9 @@ namespace Exercise1
             var file2 = "Sample.xml";
             Exercise1_2(file2);
             Console.WriteLine("-------");
-            /*var file = "Sample.xml";
-            Exercise1_3(file);
-            Console.WriteLine("-------");*/
+            var file3 = "Sample.xml";
+            Exercise1_3(file3);
+            Console.WriteLine("-------");
         }
 
         private static void Exercise1_1(string file)
@@ -48,9 +48,19 @@ namespace Exercise1
             }
         }
 
-        /*private static void Exercise1_3(string file)
+        private static void Exercise1_3(string file3)
         {
             var xdoc = XDocument.Load("Sample.xml");
-        }*/
+            var xsamples = xdoc.Root.Elements()
+                                    .Select(x=>new
+                                    {
+                                        Name = x.Element("name").Value,
+                                        Teammembers = x.Element("teammembers").Value//P284匿名クラス
+                                    })
+                                    .OrderByDescending(x=>int.Parse(x.Teammembers))
+                                    .First();
+
+            Console.WriteLine("{0}", xsamples.Name);
+        }
     }
 }
